53c61b15d9d88d477ffad0b3c2591b61a6a32624
柳耀 <liu5461yao@126.com>
2020-11-26 14:36:28 +0800

    update

--- configure.sh
+++ configure.sh
@@ -22,13 +22,16 @@ cat << EOF > /usr/local/etc/v2ray/config.json
                 "clients": [
                     {
                         "id": "$UUID",
-                        "alterId": 64
+                        "alterId": 5461
                     }
                 ],
                 "disableInsecureEncryption": true
             },
             "streamSettings": {
-                "network": "ws"
+                "network": "ws",
+                "wsSettings": {
+                    "path": "/ws"
+                }
             }
         }
     ],
